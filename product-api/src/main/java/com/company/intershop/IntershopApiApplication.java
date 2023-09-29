ff21315f0ca4f038be79561a069f1d4fdd158d02
Sergey Dolgov <saddolgov@gmail.com>
2023-09-29 22:23:57 +0300

    Initial commit

---
+++ product-api/src/main/java/com/company/intershop/IntershopApiApplication.java
@@ -0,0 +1,13 @@
+package com.company.intershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntershopApiApplication.class, args);
+	}
+
+}
---
+++ product-api/src/test/java/com/company/intershop/IntershopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.intershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
